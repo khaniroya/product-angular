a5af810440effce7eb6e06d741bf9cd0ed3ec179
roya khani <royakhanii92@gmail.com>
2023-06-22 16:04:10 +0330

    aome edit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './product-list/product-list.component';
+import { SaleProductComponent } from './product-list/presentation/sale-product/sale-product.component';
+import { ReceiptProductComponent } from './product-list/presentation/receipt-product/receipt-product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'product-list',
+    loadChildren: () =>
+      import('../app/product-list/product-list.module').then(m => m.ProductListModule),
+    component: ProductListComponent
+  },
+  {
+    path: 'product-sale',
+    loadChildren: () =>
+      import('../app/product-list/product-list.module').then(m => m.ProductListModule),
+    component: SaleProductComponent
+  },
+  {
+    path: 'product-receipt',
+    loadChildren: () =>
+      import('../app/product-list/product-list.module').then(m => m.ProductListModule),
+    component: ReceiptProductComponent
+  },
+  {
+    path: '', redirectTo: 'product-list', pathMatch: 'full'
+  },
+  {
+    path: '**', redirectTo: 'product-list', pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { ProductListModule } from './product-list/product-list.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ProductListModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
