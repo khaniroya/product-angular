9f6a89af08a9cacd7c26ed8852764da962d847e8
roya khani <royakhanii92@gmail.com>
2023-06-30 14:57:58 +0330

    product crud

---
+++ src/app/product-list/presentation/create-new-sale/create-new-sale.component.ts
@@ -0,0 +1,66 @@
+import { Component, EventEmitter, Input, OnInit, Output, SimpleChanges, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ProductCategory, ProductModel, ReceiptOrSaleModel } from '../../application/product-model';
+import { randomIdCreate } from 'src/utils/randomId';
+import { ProductReceiptService } from '../../application/product.receipt.service';
+
+@Component({
+  selector: 'app-create-new-sale',
+  templateUrl: './create-new-sale.component.html',
+  styleUrls: ['./create-new-sale.component.scss']
+})
+export class CreateNewSaleComponent implements OnInit {
+  @ViewChild('saleProduct') saleProduct: NgForm;
+  @Input() editId: any
+  @Input() catgories: ProductCategory[]
+  @Input() saleList: ReceiptOrSaleModel[]
+  @Input() productList: ProductModel[]
+  @Output() onClose = new EventEmitter<void>();
+  @Output() addItem = new EventEmitter<ReceiptOrSaleModel>();
+  editData: any
+  nullItem = {
+    id: 0,
+    name: '',
+    number: 0
+  }
+  constructor(private receiptService: ProductReceiptService) { }
+  ngOnInit() {
+    if (this.editId) {
+      this.editData = this.saleList.filter((c: any) => {
+        return c.id == this.editId
+      })
+      this.editData = this.editData[0]
+    } else {
+      this.editData = this.nullItem
+    }
+    console.log(this.productList)
+  }
+
+  close() {
+    this.onClose.emit();
+    this.editId = null
+
+  }
+
+  onSubmit() {
+    var dataForm = this.saleProduct.value;
+    if (this.saleProduct.valid && dataForm != null) {
+      if (this.editId) {
+        this.receiptService.setValueForm(this.saleProduct, dataForm)
+      } else {
+        let item: ReceiptOrSaleModel = {
+          id: randomIdCreate(0, 1000),
+          name: dataForm.name,
+          number: dataForm.number
+        }
+        this.addItem.emit(item)
+      }
+      this.onClose.emit()
+    } else {
+      alert('فرم خطا دارد ثبت نشد')
+    }
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+
+  }
+}
