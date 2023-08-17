2fbd3e674327b987e10431c1f94307b75d34e8ce
rkhani <royakhanii92@gmail.com>
2023-08-17 13:24:01 +0330

    some edit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  host: {
+    '(document:click)': 'onClick($event)',
+  },
 })
 export class AppComponent {
   title = 'app-test';
+
+  show_dp: boolean = false
+  clickToShowDropDown() {
+    this.show_dp = !this.show_dp
+  }
+  onClick(event: any) {
+    let arrayClassList = ['user', ' dropـdown', 'dropdown_menu', 'title_dp']
+    let include = arrayClassList.includes(event.srcElement.className)
+    if (!include) {
+      this.show_dp = false
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './product-list/product-list.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { ProductListModule } from './product-list/product-list.module';
